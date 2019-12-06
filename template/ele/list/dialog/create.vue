f27d073bea70a00a4d57d2b1fdc8bedb250b7d63
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2019-12-06 15:45:10 +0800

    style: 解决宽度 120 的问题，多行改为一行显示

--- template/ele/list/dialog/create.vue
+++ template/ele/list/dialog/create.vue
@@ -1,32 +1,12 @@
 <template>
   <div>
-    <el-dialog
-      width="50%"
-      title="添加会员"
-      :visible.sync="visible"
-    >
-      <el-form
-        label-width="120px"
-        :model="form"
-        :rules="rules"
-        ref="form"
-        class="adjust-form"
-      >
-        <el-form-item
-          label="会员姓名"
-          prop="name"
-        >
-          <el-input
-            v-model="form.name"
-            :maxlength="32"
-            placeholder="请输入32个字以内的会员姓名"
-          ></el-input>
+    <el-dialog width="50%" title="添加会员" :visible.sync="visible">
+      <el-form label-width="120px" :model="form" :rules="rules" ref="form">
+        <el-form-item label="会员姓名" prop="name">
+          <el-input v-model="form.name" :maxlength="32" placeholder="请输入32个字以内的会员姓名"></el-input>
         </el-form-item>
 
-        <el-form-item
-          label="证件类型"
-          prop="cardType"
-        >
+        <el-form-item label="证件类型" prop="cardType">
           <el-radio-group v-model="form.cardType">
             <el-radio :label="1">身份证</el-radio>
             <el-radio :label="2">港澳台证</el-radio>
@@ -34,10 +14,7 @@
           </el-radio-group>
         </el-form-item>
 
-        <el-form-item
-          label="会员分类"
-          prop="type"
-        >
+        <el-form-item label="会员分类" prop="type"> 
           <common-select
             v-model="form.type"
             placeholder="请选择会员分类"
@@ -51,17 +28,10 @@
         </el-form-item>
       </el-form>
 
-      <span
-        slot="footer"
-        class="dialog-footer"
-      >
+      <div slot="footer" class="dialog-footer">
         <el-button @click="onClose">取 消</el-button>
-        <el-button
-          v-loading="isLoading"
-          type="primary"
-          @click="onSubmit"
-        >确 定</el-button>
-      </span>
+        <el-button v-loading="isLoading" type="primary" @click="onSubmit">确 定</el-button>
+      </div>
     </el-dialog>
   </div>
 </template>
